84a66f6b7eca319170a5205ae8c193c1e3e9d54e
Denitsa Zhekova <denitsa.zhekova97@gmail.com>
2021-02-22 11:05:50 +0100

    Circle measurements exercise

---
+++ uni-Lesson2-Circle.py
@@ -0,0 +1,22 @@
+#import math module to get pi value
+import math
+
+def find_Diameter(radius):
+    return 2 * radius
+
+def find_Circumference(radius):
+    return 2 * math.pi * radius
+
+def find_Area(radius):
+    return math.pi * radius * radius
+
+#radius of the circle as an input from the user
+r = float(input(' Please Enter the radius of a circle: '))
+
+diameter = find_Diameter(r)
+circumference = find_Circumference(r)
+area = find_Area(r)
+
+print("\n Diameter Of a Circle = %.2f" %diameter)
+print(" Circumference Of a Circle = %.2f" %circumference)
+print(" Area Of a Circle = %.2f" %area)
