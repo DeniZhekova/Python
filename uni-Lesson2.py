12691316016e77609fbbb8bf58544d4317458b0d
Denitsa Zhekova <denitsa.zhekova97@gmail.com>
2021-02-22 09:26:50 +0100

    Create uni-Lesson2.py

---
+++ uni-Lesson2.py
@@ -0,0 +1,30 @@
+age=int(input("Enter your age: "))
+print ( "Hmm, " , age , "is a nice age to be...")
+
+color = input('What is your fav color?\n')
+print ('You fav color is %s.' % color )
+
+product = 10 * 15
+print ('The product prices is %s.' % product )
+
+total = 200
+down_payment = 75
+due = total - down_payment
+print("Your due amount is %s" %due)
+
+unitPrice = 200
+totalProducts = int(input("How many products do you want? One costs 200 dkk. : " ))
+
+totalPrice = unitPrice * totalProducts
+print ("That comes to a total of %s" % totalPrice)
+
+
+downPayment = int(input("How much can you pay now? :"))
+due2 = totalPrice - downPayment
+print("Your due amount is %s" %due2)
+
+subtotal= int(input("What is the subtotal value? : "))
+calTotal = subtotal*0.15
+print("The total is: %s" % calTotal)
+
+#DenitsaZhekova
